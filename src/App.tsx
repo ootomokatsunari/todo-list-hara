5d973776b2f984a0484e15462e7b66138558eb76
katsunariootomo <ka.mi.0512@icloud.com>
2023-04-20 18:37:23 +0900

    fix/修正"1"

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,11 @@ function App() {
   console.log(add(1, 2));
 
   //2.文字列を連結する関数を作成
-  const concat = (str1: string, str2: string) => {
-    return str1 + str2;
+  const concat = (num1: string, num2: string) => {
+    return num1 + num2;
   };
 
-  console.log(concat("ペン", "アップル"));
+  console.log(concat("はじく", "ポップコーン"));
 
   //3.引数が配列の合計値を返す関数を作成
   const sum = (nums: number[]) => {
@@ -30,51 +30,93 @@ function App() {
 
   //4.引数がオブジェクトのプロパティを参照する関数を作成
 
-  const info = {
-    name: "田中 太郎",
-    age: 30,
-    japan: true,
-    hobby: ["読書", "ウォーキング", "音楽"],
+  // const info = {
+  //   name: "田中 太郎",
+  //   age: 30,
+  //   japan: true,
+  //   hobby: ["読書", "ウォーキング", "音楽"],
+  // };
+  // const user = {
+  //   name: "田中太郎",
+  //   age: 30,
+  // };
+  // console.log(user.name + "," + user.age);
+
+  const UserInfo = (user: { name: any; age: any }) => {
+    console.log(user.name + "," + user.age);
   };
+
   const user = {
     name: "田中太郎",
     age: 30,
   };
-  console.log(user.name + "," + user.age);
+
+  UserInfo(user);
 
   //5.配列の中で最大値を返す関数を作成
-  const array = [2, 3, 56, 67, 88, 120];
-  const max = Math.max(...array);
-  console.log(max);
+  // const array = [2, 3, 56, 67, 88, 120];
+  // const max = Math.max(...array);
+  // console.log(max);
+
+  const getMax = (array: number[]) => {
+    return Math.max(...array);
+  };
+
+  console.log(getMax([2, 3, 56, 67, 88, 120]));
 
   //6.オブジェクトの配列から指定されたプロパティの値を抽出する関数を作成
-  const fruits = [
-    { name: "apple", price: 200 },
-    { name: "banana", price: 100 },
-  ];
-  console.log(fruits[1].price);
+  // const fruits = [
+  //   { name: "apple", price: 200 },
+  //   { name: "banana", price: 100 },
+  // ];
+  // console.log(fruits[1].price);
+
+  const fruit = (
+    fruits: { price: any }[] | { name: string; price: number }[]
+  ) => {
+    return fruits[1].price;
+  };
+
+  console.log(
+    fruit([
+      { name: "apple", price: 200 },
+      { name: "banana", price: 100 },
+    ])
+  );
 
   //7.オブジェクトの配列を指定されたプロパティでソートする関数を作成
-  const popcorn = [
+  // const popcorn = [
+  //   { name: "caramel", price: 200 },
+  //   { name: "solt", price: 150 },
+  //   { name: "butter", price: 180 },
+  //   { name: "hajiku", price: 3000 },
+  // ];
+  // popcorn.sort(function (a, b) {
+  //   return a.price - b.price;
+  // });
+  // console.log(JSON.stringify(popcorn));
+
+  const popcorn = () => [
     { name: "caramel", price: 200 },
     { name: "solt", price: 150 },
     { name: "butter", price: 180 },
     { name: "hajiku", price: 3000 },
   ];
-  popcorn.sort(function (a, b) {
+
+  popcorn().sort(function (a: { price: number }, b: { price: number }) {
     return a.price - b.price;
   });
-  console.log(JSON.stringify(popcorn));
+  console.log(JSON.stringify(popcorn()));
 
   //8.配列を指定された数値で分割する関数を作成する
-  const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-  const array_segmentation = [];
+  const number = () => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const newNumber = [];
 
-  while (number.length > 0) {
-    array_segmentation.push(number.splice(0, 3));
+  while (number().length > 0) {
+    newNumber.push(number().splice(0, 3));
   }
 
-  console.log(array_segmentation);
+  console.log(newNumber);
 
   return <div></div>;
 }
